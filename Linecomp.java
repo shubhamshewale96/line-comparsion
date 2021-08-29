a9bc108f8508e96c3b9ad3ff98854f3dc73d1b75
shubhamshewale96 <shubhamshewale1996@gmail.com>
2021-08-30 00:01:35 +0530

    Check equality of two line

--- Linecomp.java
+++ Linecomp.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 
 public class Linecomp {
     static double distance(int x1, int y1, int x2, int y2)
@@ -9,5 +10,8 @@ public class Linecomp {
         System.out.println("Welcome to line comparison program");
         double dis = distance(5,4,8,6);
         System.out.println("distance  "+dis);
+        double dis1=distance(3,6,8,7);
+        System.out.println("distance "+dis1);
+        System.out.println(Objects.equals(dis, dis1));
     }
 }
