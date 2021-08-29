3eeb072b0bade4a635810bed3cec1b4c122823c7
shubhamshewale96 <shubhamshewale1996@gmail.com>
2021-08-30 00:50:18 +0530

    compare two line

--- Linecomp.java
+++ Linecomp.java
@@ -1,4 +1,6 @@
+import java.util.Comparator;
 import java.util.Objects;
+import java.lang.Integer;
 
 public class Linecomp {
     static double distance(int x1, int y1, int x2, int y2)
@@ -8,10 +10,18 @@ public class Linecomp {
     }
     public static void main(String[] args) {
         System.out.println("Welcome to line comparison program");
-        double dis = distance(5,4,8,6);
+        Integer dis = (int)distance(5,4,8,6);
         System.out.println("distance  "+dis);
-        double dis1=distance(3,6,8,7);
+        Integer dis1=(int)distance(3,6,8,7);
         System.out.println("distance "+dis1);
-        System.out.println(Objects.equals(dis, dis1));
+        //System.out.println(Objects.equals(dis, dis1))
+        int comp = dis.compareTo(dis1);
+        if(comp > 0) {
+            System.out.println("line1 is greater than line2");
+        } else if(comp< 0) {
+            System.out.println("line1 is less than line2");
+        } else {
+            System.out.println("line1 is equal to line2");
+        }
     }
 }
